1f710cfa413a734c952856be5a2988810891f17b
Blaine G <bgilb@seas.upenn.edu>
2014-05-07 00:01:35 -0400

    splits up app.js into www file also handles errors

--- app.js
+++ app.js
@@ -2,13 +2,33 @@ var express = require('express');
 var logger = require('morgan');
 
 var app = express();
-app.use(logger('dev'));
-app.set('syncromatics-host', 'http://www.pennrides.com');
+app.use(logger('dev')); // dev is for color logs
 
 var routes = require('./routes');
 app.use(routes);
 
-var port = Number(process.env.PORT || 3000);
-var server = app.listen(port, function() {
-  console.log('Listening on port %d', server.address().port);
+// must be a 404 if the routes don't respond
+app.use(function(req, res, next) {
+  res.status(404);
+  res.json({error: 'Not Found'});
+})
+
+// error handling middleware for development
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    if (err) {
+      console.error(err.stack);
+      next(err);
+    } else {
+      next();
+    }
+  });
+}
+
+// render json error message
+app.use(function(err, req, res, next) {
+  res.status(err.statusCode || err.status || 500);
+  res.json({error: err.message});
 });
+
+module.exports = app;
